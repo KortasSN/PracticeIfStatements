c37ed71e43e245f22095210ba2aa94e7e71374e6
kortasthekiller <kortas@gmail.com>
2016-02-22 14:47:58 -0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,40 @@
+package com.josh;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        double inputNumber = scanner.nextDouble();
+        if (inputNumber > 100) {
+            System.out.println("The number you entered is larger than 100.");
+
+        }
+
+
+
+
+
+
+
+        /*     DONE Ask the user for a number. Print a message if the number is greater than 100.
+
+Ask the user for a number. Print a message if the number is greater than 100, and a different message if it is less than or equal to 100. Use if - else statements
+
+Ask the user for a number. Print a message if the number is greater or equal to 100.
+
+Ask the user for a number. Print a message if the number is greater than 100. Print a different message if the number is equal to 100. Print another different message if the number is less than 100. Use if - else if - else statements.
+
+Create a boolean variable and store true in it
+
+Create a boolean variable and store false in it
+
+Ask the user for their name. Print a message if their name is longer than 5 letters. (hint myString.length() gives the length of a string variable called myString)
+
+Ask the user for their name. Print a message if their name has an even number of letters. (hint use the % operator)
+
+Think up another quiz question, (like Q: What year did Apollo 11 land on the moon? A: 1969). Ask the user the question, and print a message telling them if they are right or wrong. */
+
+    }
+}
